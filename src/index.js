9d037232ea9904e96a463e408bfa2f0b00726052
NotTayyy <robertantoine99@gmail.com>
2023-01-15 17:33:28 -0500

    No More Strict Mode

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet" />
       <Router />
     </BrowserRouter>
-  </React.StrictMode>
 );
