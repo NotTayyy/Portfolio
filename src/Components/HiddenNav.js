372c147f02f64ddd5b052938e3bc96b6b4d88c41
NotTayyy <robertantoine99@gmail.com>
2023-01-15 17:35:52 -0500

    Alot of Code dealing with CSS Style and Class Manipulation

--- src/Components/HiddenNav.js
+++ src/Components/HiddenNav.js
@@ -1,48 +1,68 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function HiddenNav() {
+    const rippleMain = document.getElementById('root')
+    const navigate = useNavigate()
 
-    const animButtonRef = useRef();
+    useEffect(() => {
+        const animButtonRef = document.querySelectorAll('.nav-link');
+        animButtonRef.forEach(el => el.addEventListener('mousedown', onClick))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-    useEffect(()=>{
-        const appConProp = () => {
-            animButtonRef.current.classList.add("click-ripple");  
-        };
+    const applyStyles = (ripple, ev) => {
+        const { clientX, clientY } = ev;
+        const { style } = ripple;
+        const sideOffset = 50;
+        style.setProperty("--effect-top", `${clientY - sideOffset}px`)
+        style.setProperty("--effect-left", `${clientX - sideOffset}px`)
+    }
 
-        const onClick = () => {
-            animButtonRef.current.classList.remove('active');
-            animButtonRef.current.classList.add('active');
-        }
+    let onClick = (e) => {
+        const targetPage = e.currentTarget.getAttribute('href');
+        console.log(e.targetPage);
+        rippleMain.classList.remove('active');
+        applyStyles(rippleMain, e);
+        rippleMain.classList.add('active');
+        cleanupEvents()
+        setTimeout(() => {
+            rippleMain.classList.remove('active')
+            rippleMain.classList.remove('ripple-container')
+            navigate(targetPage)
+            window.location.reload();
+        }, 600);
+    }
 
-        appConProp();
+    function cleanupEvents() {
+        const animButtonRef = document.querySelectorAll('.nav-link');
+        animButtonRef.forEach(el => el.removeEventListener('mousedown', onClick))
+        animButtonRef.forEach(el => el.addEventListener('mousedown', onClick))
+    }
 
-        animButtonRef.current.addEventListener('mouseup', onClick);
-
-        const cleanupRef = animButtonRef.current
-
-        return () => {
-            cleanupRef.removeEventListener('mouseup', onClick)
-        }
-    })
+    let NoGo = event => {
+        event.preventDefault();
+        cleanupEvents();
+    }
 
 return (
     <div id='nav-links'>
-        <a className='nav-link' href='*'  ref={animButtonRef}>
+        <a className='nav-link ripple-container' href='/About' onClick={NoGo}>
             <h2 className='nav-link-label'>About</h2>
-            <img className='nav-link-image' src='https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8&w=1000&q=80' alt='About_Page'></img>
+            <img className='nav-link-image' src='' alt='About_Page'></img>
         </a>
-        <a className='nav-link' href='/Portfolio' ref={animButtonRef}>
+        <a className='nav-link ripple-container' href='/' onClick={NoGo}>
             <h2 className='nav-link-label'>Projects</h2>
             <img className='nav-link-image' src='https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8&w=1000&q=80' alt='Projects_Page'></img>
         </a>
-        <a className='nav-link' href='/Portfolio' ref={animButtonRef}>
+        <a className='nav-link ripple-container' href='/' onClick={NoGo}>
             <h2 className='nav-link-label'>Skills</h2>
-            <img className='nav-link-image' src='https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8&w=1000&q=80' alt='Skills_Page'></img>
+            <img className='nav-link-image' src='https://miro.medium.com/max/1400/1*u0WymkmJT6Bu6kGQ10G_5Q.png' alt='Skills_Page'></img>
         </a>
-        <a className='nav-link' href='/Portfolio' ref={animButtonRef}>
+        <a className='nav-link ripple-container' href='/' onClick={NoGo}>
             <h2 className='nav-link-label'>Contact</h2>
-            <img className='nav-link-image' src='https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8&w=1000&q=80' alt='Contacts_Page'></img>
+            <img className='nav-link-image' src='https://www.howtogeek.com/wp-content/uploads/2018/07/img_5b58e0efdc4b9.jpg?height=200p&trim=2,2,2,2' alt='Contacts_Page'></img>
         </a>
     </div>
-)
+    )
 }
